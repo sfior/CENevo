db986855629c478d0b7e0f1b7e5e2f4c37fdc9bc
sfior <simonefior@hotmail.com>
2024-03-07 20:32:08 +0100

    Add files via upload

---
+++ Haplotype data/CEN haplotypes/Proteins/Focal_pops/2_geo_mapper.R
@@ -0,0 +1,41 @@
+# This scipt plots frequencies of protein variants as pie charts on a map
+
+# Import libraries
+library(maps)
+library(mapdata)
+library(maptools)  #for shapefiles
+library(scales)  #for transparency
+library(mapplots)
+library(raster)
+library(RColorBrewer)
+
+# Import data
+data<-read.table("Proteins_freqs.txt",header=T,sep='\t')
+
+my_col<-c(alpha('blue', 0.6), alpha('green', 0.6), alpha('red',0.6), alpha('black',0.6),alpha('magenta',0.6),alpha('orange',0.6))
+
+
+# Download map of Switzerland
+swiss_dem <- getData('alt' , country='CHE', mask=TRUE)
+swiss <- getData('GADM' , country="CHE", level=0)
+# Or for a high resolution DEM
+alps.hiRes <- readRDS("ELEV.GMTED.alps.hiRes.rds")
+
+plot(swiss_dem)
+#plot(alps.hiRes, col = brewer.pal(9, "Greys"))
+
+# Crop
+e <- extent(7, 8, 45.8, 46.5)
+#switzerland <- crop(swiss_dem, e)
+switzerland <- crop(alps.hiRes, e)
+plot(switzerland, col=rev(grey(0:100/100)))
+
+for (n in 1:nrow(data)){
+  line<-data[n,]
+  add.pie(z=c(line[,5],line[,6],line[,7],line[,8],line[,9],line[,10]), x=line[,3], y=line[,2], radius=sqrt(line[,4]/4000), col=my_col, labels='')
+  
+}
+
+
+
+
