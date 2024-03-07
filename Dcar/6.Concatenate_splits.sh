046065c346a97a50a6af1ec1d6158fe633869e7f
Hirzi Luqman <zanzzibarbarian@gmail.com>
2024-03-07 13:49:53 +0000

    Add files via upload

---
+++ Dcar/6.Concatenate_splits.sh
@@ -0,0 +1,7 @@
+### Concatenate split regions into single (Chr 16 file)
+
+head -n1 split_1/*1kbOverlapWindows.obs > Chr16_allScaffolds_FullSS_start1_end9999999_windowsize5000_M2_Q0.obs
+for i in `seq 1 20`; do
+	tail -n+2 split_${i}/*1kbOverlapWindows.obs >> Chr16_allScaffolds_FullSS_start1_end9999999_windowsize5000_M2_Q0.obs;
+	cat split_${i}/*1kbOverlapWindows.scaffoldlist >> Chr16_allScaffolds_FullSS_start1_end9999999_windowsize5000_M2_Q0.scaffoldlist;
+done
