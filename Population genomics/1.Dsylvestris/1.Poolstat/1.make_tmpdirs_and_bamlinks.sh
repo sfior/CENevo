d11eca0ce6941f98204f5e7319aed41dc6f00c66
sfior <simonefior@hotmail.com>
2024-03-12 12:57:17 +0100

    Create 1.make_tmpdirs_and_bamlinks.sh

---
+++ Population genomics/1.Dsylvestris/1.Poolstat/1.make_tmpdirs_and_bamlinks.sh
@@ -0,0 +1,17 @@
+# This scipt makes temporary folders and links bam files used downstream
+
+
+DIR='xxx'  # specify path of directory containign bam files
+
+NJOBS=200
+
+for n in $(seq 1 $NJOBS)
+do
+T='./tmp.'${n}'/'
+mkdir ${T}
+for i in $(ls -1 ${DIR}*sorted.bam* |  awk -F '/' '{print $NF}' )
+do
+#echo ${i}
+ln -s ${DIR}${i} ${T}${i}
+done
+done
