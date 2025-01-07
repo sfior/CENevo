db26a99c15ea8c47ab06f1dee142c8c7741d5f50
sfior <simonefior@hotmail.com>
2025-01-07 16:16:31 +0100

    file upload

--- Ecology/1.Phenotypic_traits/01_phenoTraits_plotsNstats.R
+++ Ecology/1.Phenotypic_traits/01_phenoTraits_plotsNstats.R
@@ -4,7 +4,7 @@ library(emmeans)
 
 ##### PCA
 # Dsylvestris
-eschikon.mat<-read.table('Dsyl_phenoTraits.txt',sep='\t',header=T,na.string='NA')
+eschikon.mat<-read.table('01_Dsyl_phenoTraits.txt',sep='\t',header=T,na.string='NA')
 traits<-c(3:17)
 eschikon.pops<-eschikon.mat[,1]
 eschikon.pops <- factor(eschikon.pops, levels = c("Saxon", "Saviese", "Varen","Tsanfleuron", "Challer", "Mauvoisin"))
@@ -28,7 +28,7 @@ print(g)
 
 
 #Dcarthusianorum
-eschikon.mat<-read.table('Dcar_phenoTraits.txt',sep='\t',header=T,na.string='NA')
+eschikon.mat<-read.table('01_Dcar_phenoTraits.txt',sep='\t',header=T,na.string='NA')
 traits<-c(3:17)
 eschikon.pops<-eschikon.mat[,1]
 eschikon.pops <- factor(eschikon.pops, levels = c("Niedergampel", "Unterstalden", "Grengiols","Faldumalp", "Gibidumsee", "Simplonpass"))
@@ -52,8 +52,8 @@ print(g)
 
 
 # combined Dsylvestris + Dcarthusian
-data1<-read.table('Dsyl_phenoTraits.txt',sep='\t',header=T,na.string='NA')
-data2<-read.table('Dcar_phenoTraits.txt',sep='\t',header=T,na.string='NA')
+data1<-read.table('01_Dsyl_phenoTraits.txt',sep='\t',header=T,na.string='NA')
+data2<-read.table('01_Dcar_phenoTraits.txt',sep='\t',header=T,na.string='NA')
 # make Z scores
 data1Z<-data1
 data1Z[3:17]<-scale(data1Z[3:17])
@@ -74,14 +74,14 @@ print(g)
 
 ######  Boxplots and statistics flowering time
 ### Dsylvestris
-eschikon.mat<-read.table('Dsyl_phenoTraits.txt',sep='\t',header=T,na.string='NA')
-eschikon.mat$Population <- factor(eschikon.mat$Population, levels = c("Saxon", "Saviese", "Varen","Tsanfleuron", "Challer", "Mauvoisin"))
+eschikon.mat<-read.table('01_Dsyl_phenoTraits.txt',sep='\t',header=T,na.string='NA')
+eschikon.mat$Population <- factor(eschikon.mat$Population, levels = c("Saxon","Varen","Saviese", "Tsanfleuron", "Challer", "Mauvoisin"))
 # log transform
-eschikon.mat[3]<-log(eschikon.mat[3])
+eschikon.mat[,3]<-log(as.numeric(eschikon.mat[,3]))
 # remove 2SD oultiers
 tmp1<-cbind(eschikon.mat[,c(1,3)])
 y<-c()
-pops<-c("Saxon", "Saviese", "Varen","Tsanfleuron", "Challer", "Mauvoisin")
+pops<-c("Saxon","Varen","Saviese", "Tsanfleuron", "Challer", "Mauvoisin") # must be in the same order as in table
 for (p in pops){
   tmp2<-subset(tmp1,Population==p)
   x<-tmp2[,2]
@@ -89,16 +89,19 @@ for (p in pops){
   x[x < (mean(x,na.rm=T)-(2*sd(x,na.rm=T)))] <- NA
   y<-c(y,x)
 }
-mat<-cbind(eschikon.mat$Population,y)
-colnames(mat)<-colnames(eschikon.mat[c(1,3)])
+mat <- data.frame(
+  Population = eschikon.mat$Population,
+  Flowering_date.d. = y
+)
+mat$Population <- factor(mat$Population, levels = c("Saxon","Varen","Saviese", "Tsanfleuron", "Challer", "Mauvoisin"))
 # make plots
-boxplot(Flowering_date ~ Population, data = mat, col = c('red', 'red', 'red', 'blue', 'blue', 'blue'))
+boxplot(Flowering_date.d. ~ Population, data = mat, col = c('red', 'red', 'red', 'blue', 'blue', 'blue'),las=2)
 #Test difference in flowering time between high- and low-elevation
 mat<-data.frame(cbind(mat,eschikon.mat$Ecotype,eschikon.mat$Motherline))
 colnames(mat)[3:4]<-c('Ecotype','Motherline')
-mat$Flowering_date<-as.numeric(as.character(mat$Flowering_date))
-m1<-lmer(Flowering_date ~ Ecotype + (1|Population/Motherline), data=mat,  na.action=na.exclude)
-m2 <- lmer(Flowering_date ~ 1 + (1|Population/Motherline), data=mat,  na.action=na.exclude)
+mat$Flowering_date.d.<-as.numeric(as.character(mat$Flowering_date.d.))
+m1<-lmer(Flowering_date.d. ~ Ecotype + (1|Population/Motherline), data=mat,  na.action=na.exclude)
+m2 <- lmer(Flowering_date.d. ~ 1 + (1|Population/Motherline), data=mat,  na.action=na.exclude)
 anova(m1,m2)
 lsmeans(m1, 'Ecotype')
 emm <- emmeans(m1, "Ecotype")
@@ -106,14 +109,14 @@ pairs(emm)
 
 
 ### Dcarthusianorum
-eschikon.mat<-read.table('Dcar_phenoTraits.txt',sep='\t',header=T,na.string='NA')
-eschikon.mat$Population <- factor(eschikon.mat$Population, levels = c("Niedergampel", "Unterstalden", "Grengiols","Faldumalp", "Gibidumsee", "Simplonpass"))
+eschikon.mat<-read.table('01_Dcar_phenoTraits.txt',sep='\t',header=T,na.string='NA')
+eschikon.mat$Population <- factor(eschikon.mat$Population, levels = c("Unterstalden","Niedergampel","Grengiols","Gibidumsee","Faldumalp","Simplonpass"))
 # log transform
-eschikon.mat[3]<-log(eschikon.mat[3])
+eschikon.mat[,3]<-log(eschikon.mat[,3])
 # remove 2SD oultiers
 tmp1<-cbind(eschikon.mat[,c(1,3)])
 y<-c()
-pops<-c("Niedergampel", "Unterstalden", "Grengiols","Faldumalp", "Gibidumsee", "Simplonpass")
+pops<-c("Unterstalden","Niedergampel","Grengiols","Gibidumsee","Faldumalp","Simplonpass") # must be in the same order as in table
 for (p in pops){
   tmp2<-subset(tmp1,Population==p)
   x<-tmp2[,2]
@@ -121,16 +124,19 @@ for (p in pops){
   x[x < (mean(x,na.rm=T)-(2*sd(x,na.rm=T)))] <- NA
   y<-c(y,x)
 }
-mat<-cbind(eschikon.mat$Population,y)
-colnames(mat)<-colnames(eschikon.mat[c(1,3)])
+mat <- data.frame(
+  Population = eschikon.mat$Population,
+  Flowering_date.d. = y
+)
+mat$Population <- factor(mat$Population, levels = c("Unterstalden","Niedergampel","Grengiols","Gibidumsee","Faldumalp","Simplonpass"))
 # make plots
-boxplot(Flowering_date ~ Population, data = mat, col = c('red', 'red', 'red', 'blue', 'blue', 'blue'))
+boxplot(Flowering_date.d. ~ Population, data = mat, col = c('red', 'red', 'red', 'blue', 'blue', 'blue'),las=2)
 #Statistics for difference in flowering time between high- and low-elevation
 mat<-data.frame(cbind(mat,eschikon.mat$Ecotype,eschikon.mat$Motherline))
 colnames(mat)[3:4]<-c('Ecotype','Motherline')
-mat$Flowering_date<-as.numeric(as.character(mat$Flowering_date))
-m1<-lmer(Flowering_date ~ Ecotype + (1|Population/Motherline), data=mat,  na.action=na.exclude)
-m2 <- lmer(Flowering_date ~ 1 + (1|Population/Motherline), data=mat,  na.action=na.exclude)
+mat$Flowering_date.d.<-as.numeric(as.character(mat$Flowering_date.d.))
+m1<-lmer(Flowering_date.d. ~ Ecotype + (1|Population/Motherline), data=mat,  na.action=na.exclude)
+m2 <- lmer(Flowering_date.d. ~ 1 + (1|Population/Motherline), data=mat,  na.action=na.exclude)
 anova(m1,m2)
 lsmeans(m1, 'Ecotype')
 emm <- emmeans(m1, "Ecotype")
